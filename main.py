953789688f8729988a71515ea72dde1ee6cfa5a9
frankycode <franpadron26@gmail.com>
2025-03-22 11:36:15 +0000

    feat: add constants and open close the black screen

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+
+
+def setupScreen():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    background_color = (0, 0, 0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, background_color)
+        pygame.display.flip()
+
+
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    setupScreen()
 
 if __name__ == "__main__":
     main()
