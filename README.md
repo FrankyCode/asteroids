7c1734788c0f6665a3bf6f6ad5fc38b8954e83b9
frankycode <franpadron26@gmail.com>
2025-03-23 21:28:22 +0000

    docs: update README with gameplay instructions and documentation links; refactor main.py for screen setup and frame rate control

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# asteroids
+# Asteroids
 Asteroids game in Python
+
+# How to play?
+
+
+## DOC
+- pygame: https://www.pygame.org/docs/ref/pygame.html
--- main.py
+++ main.py
@@ -4,21 +4,28 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 
 
 def setupScreen():
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    background_color = (0, 0, 0)
+    clock =  pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, background_color)
+        
+        screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     setupScreen()
 
 if __name__ == "__main__":
