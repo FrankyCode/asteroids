ace6b76397ba3db6a6db430da9da9fd7f33f1cb0
frankycode <franpadron26@gmail.com>
2025-03-23 21:49:51 +0000

    feat: integrate Player class into main.py for player representation and rendering

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
-
+from player import Player
 
 def setupScreen():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock =  pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -17,6 +18,7 @@ def setupScreen():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
